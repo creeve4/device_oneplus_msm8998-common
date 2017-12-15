160389db4020ae326fcbf4789c24f89e259366b2
liquid0624 <rjbirnesser@gmail.com>
2017-12-15 18:08:33 -0500

    msm8998-common: disable camera parameters

--- camera/Android.mk
+++ camera/Android.mk
@@ -14,19 +14,19 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
+#LOCAL_C_INCLUDES := \
     frameworks/av/include
 
-LOCAL_SRC_FILES := \
+#LOCAL_SRC_FILES := \
     CameraParameters.cpp
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE := libcamera_parameters_ext
-LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+#LOCAL_MODULE := libcamera_parameters_ext
+#LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_STATIC_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
