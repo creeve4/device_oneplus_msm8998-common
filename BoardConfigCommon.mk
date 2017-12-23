fd72aa3a5fc52b7a6d57aa738988507310b18441
martinusbe <martinusbe@gmail.com>
2017-12-23 13:48:31 +0100

    common: give sepolicy a path
     * and fix build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -265,7 +265,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS := device/oneplus/msm8998-common/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
