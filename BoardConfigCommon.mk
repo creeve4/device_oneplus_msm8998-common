e18d31b2a43cd2888c7329958b5e05a5b6595f8d
martinusbe <martinusbe@gmail.com>
2017-12-23 11:55:25 +0100

    common: cleanup & enable device sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -265,7 +265,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
